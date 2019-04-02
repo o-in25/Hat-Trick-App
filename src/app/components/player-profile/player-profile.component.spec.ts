bcf523b8327a77794c3aa1b615998f72be4add78
Eoin Halligan <ehalligan12@gmail.com>
2019-04-01 22:58:14 -0400

    added models

--- src/app/components/player-profile/player-profile.component.spec.ts
+++ src/app/components/player-profile/player-profile.component.spec.ts
@@ -1,6 +1,7 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+// @ts-ignore
 import { PlayerProfilePage } from './player-profile.page';
 
 describe('PlayerProfilePage', () => {
--- src/models/player.ts
+++ src/models/player.ts
@@ -1,5 +1,5 @@
 export class CurrentTeam {
-    id: Number;
+    id: number;
     playingProbability: string
 }
 
@@ -22,18 +22,18 @@ export class MediaAccount {
 }
 
 export class Information {
-    id: Number;
+    id: number;
     firstName: string;
     lastName: string;
     primaryPosition: string;
-    jerseyNumber: Number;
+    jerseyNumber: number;
     currentTeam: CurrentTeam;
     currentRosterStatus: string;
     currentInjury: CurrentInjury;
     height: string;
-    weight: Number;
+    weight: number;
     birthDate: string;
-    age: Number;
+    age: number;
     birthCity: string;
     birthCountry: string;
     rookie: boolean;
@@ -44,6 +44,103 @@ export class Information {
     socialMediaAccounts: [SocialMediaAccounts]
 }
 
+export class FieldGoals {
+    fg2PtAtt: number;
+    fg2PtAttPerGame: number;
+    fg2PtMade: number;
+    fg2PtMadePerGame: number;
+    fg2PtPct: number;
+    fg3PtAtt: number;
+    fg3PtAttPerGame: number;
+    fg3PtMade: number;
+    fg3PtMadePerGame: number;
+    fg3PtPct: number;
+    fgAtt: number;
+    fgAttPerGame: number;
+    fgMade: number;
+    fgMadePerGame: number;
+    fgPct: number;
+}
+
+export class FreeThrows {
+    ftAtt: number;
+    ftAttPerGame: number;
+    ftMade: number;
+    ftMadePerGame: number;
+    ftPct: number;
+}
+
+export class Rebounds {
+    offReb: number;
+    offRebPerGame: number;
+    defReb: number;
+    defRebPerGame: number;
+    reb: number;
+    rebPerGame: number;
+}
+
+export class Offense {
+    ast: number;
+    astPerGame: number;
+    pts: number;
+    ptsPerGame: number;
+}
+
+export class Defense {
+    tov: number;
+    tovPerGame: number;
+    stl: number;
+    stlPerGame: number;
+    blk: number;
+    blkPerGame: number;
+    blkAgainst: number;
+    blkAgainstPerGame: number;
+}
+
+export class Miscellaneous {
+    fouls: number;
+    foulsPerGame: number;
+    foulsDrawn: number;
+    foulsDrawnPerGame: number;
+    foulPers: number;
+    foulPersPerGame: number;
+    foulPersDrawn: number;
+    foulPersDrawnPerGame: number;
+    foulTech: number;
+    foulTechPerGame: number;
+    foulTechDrawn : number;
+    foulTechDrawnPerGame: number;
+    foulFlag1: number;
+    foulFlag1PerGame: number;
+    foulFlag1Drawn: number;
+    foulFlag1DrawnPerGame: number;
+    foulFlag2: number;
+    foulFlag2PerGame: number;
+    foulFlag2Drawn: number;
+    foulFlag2DrawnPerGame: number;
+    ejections: number;
+    plusMinus: number;
+    plusMinusPerGame: number;
+    minSeconds: number;
+    minSecondsPerGame: number;
+    gamesStarted: number;
+}
+
+export class Team {
+    id: number;
+    abbreviation: string;
+}
+
+export class Stats {
+    gamesPlayed: number;
+    fieldGoals: FieldGoals;
+    freeThrows: FreeThrows;
+    rebounds: Rebounds;
+    offense: Offense;
+    defense: Defense;
+    miscellaneous: Miscellaneous;
+    
+}
 
 
 // a new player
@@ -51,5 +148,5 @@ export class Player {
     lastUpdatedOn: String;
     player: Information;
     team: Object;
-    stats: Object;
+    stats: Stats;
 }
