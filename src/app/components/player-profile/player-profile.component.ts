4bbd438ca1fc236ca2a9815edf1b7753bd0e2f1e
Eoin Halligan <ehalligan12@gmail.com>
2019-04-09 03:11:18 -0400

    added infinite scroll

--- src/app/components/player-profile/player-profile.component.ts
+++ src/app/components/player-profile/player-profile.component.ts
@@ -11,11 +11,11 @@ import {trigger, state, style, animate, transition} from "@angular/animations";
         'enterAnimation', [
           transition(':enter', [
             style({transform: 'translateX(100%)', opacity: 0}),
-            animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
+            animate('100ms', style({transform: 'translateX(0)', opacity: 1}))
           ]),
           transition(':leave', [
             style({transform: 'translateX(0)', opacity: 1}),
-            animate('500ms', style({transform: 'translateX(100%)', opacity: 0}))
+            animate('100ms', style({transform: 'translateX(100%)', opacity: 0}))
           ])
         ]
     )
--- src/app/pages/player-stats/player-stats.page.ts
+++ src/app/pages/player-stats/player-stats.page.ts
@@ -9,15 +9,39 @@ import {Player} from "../../../models/player";
 })
 export class PlayerStatsPage implements OnInit {
   players: Player[] = undefined;
-  stats: Object;
+  dataList: Player[] = undefined;
+  index: number = 0;
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) {
+      this.dataList = [];
+      // load 25 in;
+  }
+
+  loadData(event) {
+      setTimeout(() => {
+        console.log('Done');
+        for(let i = 0; i++ < 25; i++) {
+            this.dataList.push(this.players[i]);
+        }
+        event.target.complete();
+
+        // App logic to determine if all data is loaded
+        // and disable the infinite scroll
+        if(this.dataList.length == 1000) {
+          event.target.disabled = true;
+        }
+      }, 500);
+  }
 
   ngOnInit() {
-    this.apiService.getPlayerWithId(15286).subscribe((data: any) => {
+    this.apiService.getPlayers().subscribe((data: any) => {
       // some arbitrary player to test
       this.players = data;
+      for(let i = 0; i < 25; i++) {
+        this.dataList.push(this.players[i]);
+      }
     });
+
   }
 
 }
