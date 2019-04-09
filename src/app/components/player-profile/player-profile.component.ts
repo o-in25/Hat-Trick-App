afb775c6b4ab700e71146e09c7ba7432f554b05e
Eoin Halligan <ehalligan12@gmail.com>
2019-04-09 00:17:19 -0400

    adding animations

--- src/app/components/player-profile/player-profile.component.ts
+++ src/app/components/player-profile/player-profile.component.ts
@@ -1,10 +1,25 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Player} from "../../../models/player";
+import {trigger, state, style, animate, transition} from "@angular/animations";
 
 @Component({
   selector: 'app-player-profile',
   templateUrl: './player-profile.component.html',
   styleUrls: ['./player-profile.component.scss'],
+  animations: [
+    trigger(
+        'enterAnimation', [
+          transition(':enter', [
+            style({transform: 'translateX(100%)', opacity: 0}),
+            animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
+          ]),
+          transition(':leave', [
+            style({transform: 'translateX(0)', opacity: 1}),
+            animate('500ms', style({transform: 'translateX(100%)', opacity: 0}))
+          ])
+        ]
+    )
+  ]
 })
 export class PlayerProfileComponent implements OnInit {
   @Input() player: Player;
--- src/app/components/progress-circle/progress-circle.component.ts
+++ src/app/components/progress-circle/progress-circle.component.ts
@@ -4,18 +4,6 @@ import {trigger, state, style, animate, transition} from "@angular/animations";
   selector: 'app-progress-circle',
   templateUrl: './progress-circle.component.html',
   styleUrls: ['./progress-circle.component.scss'],
-  animations: [
-    trigger('elementState', [
-      state('opaque', style({
-        opacity: 1
-      })),
-      state('transparent', style({
-        opacity: 0
-      })),
-      transition('opaque => transparent', animate('4000ms ease-in')),
-      transition('transparent => opaque', animate('4000ms ease-out'))
-    ])
-  ]
 })
 export class ProgressCircleComponent implements OnInit {
   @Input() circumference: number;
