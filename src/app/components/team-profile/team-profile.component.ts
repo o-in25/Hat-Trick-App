e1cdfceef4daacb01e55001db5844c9c1ce6705e
Eoin Halligan <ehalligan12@gmail.com>
2019-04-17 08:28:54 -0400

    fixed ngIf class

--- src/app/components/team-profile/team-profile.component.ts
+++ src/app/components/team-profile/team-profile.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ChangeDetectorRef} from '@angular/core';
 import {Team} from "../../../models/team";
 
 @Component({
@@ -8,15 +8,29 @@ import {Team} from "../../../models/team";
 })
 export class TeamProfileComponent implements OnInit {
   @Input() team: Team;
+  // the number for the alternate
+  switch: number;
+  set: boolean;
+  private _changeDetectionRef : ChangeDetectorRef
 
-  constructor() { }
+  constructor() {   
+
+   }
 
   convertWinningPercentage(winPct): number {
     return Math.ceil(winPct * 100);
   }
 
+  alternate(): boolean {
+    this.set = ((this.switch % 2) == 0);
+    return this.set;
+  }
 
-  ngOnInit() {}
+
+  ngOnInit() {
+    this.switch = 0;
+    this.set = true;
+  }
 
 }
 
